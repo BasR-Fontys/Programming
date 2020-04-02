9027c33738b29c98fd73b5353a6fcfd7b2c77e65
BasR-Fontys <62379879+BasR-Fontys@users.noreply.github.com>
2020-04-02 19:43:33 +0200

    Commend error verholpen
    Door het plaatsen van commands en het verbeteren van de opmaak waren 2 lists gebroken.
    Het is nu hersteld

--- WachtwoordGenerator/WachtwoordGenerator.py
+++ WachtwoordGenerator/WachtwoordGenerator.py
@@ -19,10 +19,10 @@ else:  # sluit het programma af wanneer de gebruiker geen wachtwoord wil generer
 
 
 # Mogelijke characters voor het wachtwoord.
-Kleineletters_Char = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                      't', 'u', 'v', 'w', 'x', 'y', 'z'}
-Hoofdletters_Char = {"A", "B", "C", "D", "E", "F", "G", "H", "I", 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                     'U', 'V', 'W', 'X', 'Y', 'Z'}
+Kleineletters_Char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                      't', 'u', 'v', 'w', 'x', 'y', 'z']
+Hoofdletters_Char = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                     'U', 'V', 'W', 'X', 'Y', 'Z']
 Nummers_Char = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 Speciale_tekens_Char = ['!', '@', '#', '$', '%', '&', '*', '(', ')']
 
